2fdd9bfe1657c539172c498adcb2f7c98edbf8e3
Katie Dektar <katydek@google.com>
2020-12-11 16:42:15 -0800

    Remove license header from main.cc

--- main.cc
+++ main.cc
@@ -1,12 +1,7 @@
-// Copyright 2020 Paul Salvador Inventado and Google LLC
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
 #include "cpputils/karel/karel.h"
 
 void KarelProgram() {
-  // TODO: Your Karel code here.
+  // TODO: Place your Karel code here.
 }
 
 int main() {
